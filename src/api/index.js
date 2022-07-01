927db40b0b5d02af8aa0f65ba6ffa4478f43d57f
Tomas Hromnik <mac@Tomass-MacBook-Air.local>
2022-07-01 17:16:39 +0200

    Add https API URL for production

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
+const DEVELOPMENT_API_URL = `http://omdbapi.com/?apikey=${process.env.VUE_APP_API_KEY}`;
+const PRODUCTION_API_URL = `https://omdbapi.com/?apikey=${process.env.VUE_APP_API_KEY}`
+
 export default axios.create({
-  baseURL: `http://omdbapi.com/?apikey=${process.env.VUE_APP_API_KEY}`,
+  baseURL: process.env.NODE_ENV === 'development' ? DEVELOPMENT_API_URL : PRODUCTION_API_URL,
   timeout: 5000,
 });
